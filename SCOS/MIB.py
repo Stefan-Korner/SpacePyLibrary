5073341ef1fb4689ce6bac38c100b4747c0c7fb6
Stefan Korner <korner-hajek@gmx.at>
2015-04-18 11:55:24 +0200

    fix invalid comment

--- SCOS/MIB.py
+++ SCOS/MIB.py
@@ -11,7 +11,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser     *
 # General Public License for more details.                                    *
 #******************************************************************************
-# SCOS-2000 Functionality - Environment                                       *
+# SCOS - Mission Database (MIB) handling                                      *
 #******************************************************************************
 import SCOS.ENV
 
