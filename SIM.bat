d8f8aec4f068c976e5f30ed51d2264140ad1fc02
Stefan-Korner <korner-hajek@gmx.at>
2017-05-23 13:48:15 +0200

    add scripts for Windows

---
+++ SIM.bat
@@ -0,0 +1,41 @@
+::*****************************************************************************
+:: (C) 2017, Stefan Korner, Austria                                           *
+::                                                                            *
+:: The Space Python Library is free software; you can redistribute it and/or  *
+:: modify it under the terms of the GNU Lesser General Public License as      *
+:: published by the Free Software Foundation; either version 2.1 of the       *
+:: License, or (at your option) any later version.                            *
+::                                                                            *
+:: The Space Python Library is distributed in the hope that it will be useful,*
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser    *
+:: General Public License for more details.                                   *
+::*****************************************************************************
+:: Start scrip for the SIM simulator.                                         *
+::*****************************************************************************
+set HOST=127.0.0.1
+set NCTRS_ADMIN_SERVER_PORT=32010
+set NCTRS_TC_SERVER_PORT=32009
+set NCTRS_TM_SERVER_PORT=22104
+set TC_ACK_ACCEPT_SUCC_MNEMO=ACK1
+set TC_ACK_ACCEPT_FAIL_MNEMO=NAK1
+set TC_ACK_EXESTA_SUCC_MNEMO=ACK1
+set TC_ACK_EXESTA_FAIL_MNEMO=NAK1
+set TC_ACK_EXEPRO_SUCC_MNEMO=ACK1
+set TC_ACK_EXEPRO_FAIL_MNEMO=NAK1
+set TC_ACK_EXECUT_SUCC_MNEMO=ACK1
+set TC_ACK_EXECUT_FAIL_MNEMO=NAK1
+# see also CMD_REP_APPDATA_OFFSET
+set TC_ACK_APID_PARAM_BYTE_OFFSET=18
+set TC_ACK_SSC_PARAM_BYTE_OFFSET=20
+set TC_TT_TIME_BYTE_OFFSET=11
+set TC_TT_FINE_TIME_BYTE_SIZE=2
+set TC_TT_PKT_BYTE_OFFSET=17
+set TM_CYCLIC_MNEMO=TM_PKT1
+set TM_TT_TIME_BYTE_OFFSET=10
+set TM_TT_FINE_TIME_BYTE_SIZE=4
+set TM_RECORD_FORMAT=NCTRS
+set SYS_COLOR_LOG=1
+set scosii_homedir=D:\Programming\SpacePyLibrary\scosii_homedir
+set PYTHONPATH=D:\Programming\SpacePyLibrary;D:\Programming\SpacePyLibrary\UnitTest
+python SIM.py dummy
---
+++ clean.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#******************************************************************************
+# (C) 2017, Stefan Korner, Austria                                            *
+#                                                                             *
+# The Space Python Library is free software; you can redistribute it and/or   *
+# modify it under the terms of the GNU Lesser General Public License as       *
+# published by the Free Software Foundation; either version 2.1 of the        *
+# License, or (at your option) any later version.                             *
+#                                                                             *
+# The Space Python Library is distributed in the hope that it will be useful, *
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser     *
+# General Public License for more details.                                    *
+#******************************************************************************
+# Deletes temporary files that are created during runtime.                    *
+#******************************************************************************
+rm -f */*.pyc
+rm -f scosii_homedir/testbin/testdata.sim
