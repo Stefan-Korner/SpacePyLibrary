367f96e69bb401f716e5f692fa3b9799335d2a79
Stefan-Korner <korner-hajek@gmx.at>
2018-09-23 19:13:13 +0200

    fix unit test for TM packet creation

--- UnitTest/testSPACE.py
+++ UnitTest/testSPACE.py
@@ -15,8 +15,47 @@
 # Space Segment - Unit Tests                                                  *
 #******************************************************************************
 import testData
-import SCOS.ENV
 import SPACE.DEF, SPACE.TMGEN
+import UTIL.SYS
+
+#############
+# constants #
+#############
+SYS_CONFIGURATION = [
+  ["HOST", "127.0.0.1"],
+  ["NCTRS_ADMIN_SERVER_PORT", "13006"],
+  ["NCTRS_TC_SERVER_PORT", "13007"],
+  ["NCTRS_TM_SERVER_PORT", "2502"],
+  ["SPACECRAFT_ID", "758"],
+  ["DEF_GROUND_STATION_ID", "10"],
+  ["GROUND_STATION_NAME", "ESA G/S "],
+  ["TC_ACK_ACCEPT_SUCC_MNEMO", "ACK1"],
+  ["TC_ACK_ACCEPT_FAIL_MNEMO", "NAK1"],
+  ["TC_ACK_EXESTA_SUCC_MNEMO", "ACK2"],
+  ["TC_ACK_EXESTA_FAIL_MNEMO", "NAK2"],
+  ["TC_ACK_EXEPRO_SUCC_MNEMO", "ACK3"],
+  ["TC_ACK_EXEPRO_FAIL_MNEMO", "NAK3"],
+  ["TC_ACK_EXECUT_SUCC_MNEMO", "ACK4"],
+  ["TC_ACK_EXECUT_FAIL_MNEMO", "NAK4"],
+  ["TC_ACK_APID_PARAM_BYTE_OFFSET", "18"],
+  ["TC_ACK_SSC_PARAM_BYTE_OFFSET", "20"],
+  ["TC_TT_TIME_FORMAT", "CUC4"],
+  ["TC_TT_TIME_BYTE_OFFSET", "11"],
+  ["TC_TT_PKT_BYTE_OFFSET", "17"],
+  ["TM_CYCLIC_MNEMO", "TM_PKT1"],
+  ["TM_CYCLIC_PERIOD_MS", "5000"],
+  ["TM_TT_TIME_FORMAT", "CUC4"],
+  ["TM_TT_TIME_BYTE_OFFSET", "10"],
+  ["TM_RECORD_FORMAT", "NCTRS"],
+  ["TM_REPLAY_KEY", "SPID"],
+  ["OBT_MISSION_EPOCH_STR", UTIL.TCO.TAI_MISSION_EPOCH_STR],
+  ["OBT_LEAP_SECONDS", str(UTIL.TCO.GPS_LEAP_SECONDS_2017)],
+  ["ERT_MISSION_EPOCH_STR", UTIL.TCO.TAI_MISSION_EPOCH_STR],
+  ["ERT_LEAP_SECONDS", str(UTIL.TCO.GPS_LEAP_SECONDS_2017)],
+  ["SYS_COLOR_LOG", "1"],
+  ["SYS_APP_MNEMO", "SIM"],
+  ["SYS_APP_NAME", "Simulator"],
+  ["SYS_APP_VERSION", "1.0"]]
 
 #############
 # functions #
@@ -43,37 +82,38 @@ def test_DEFoperations():
 def test_TMGENoperations():
   """function to test the TMGEN telemetry generator"""
   SPACE.TMGEN.init()
-  # TM_PACKET_02 has a checksum
-  tmPacket = SPACE.IF.s_tmPacketGenerator.getTMpacket(SCOS.ENV.TPKT_PKT_IDLE_FRAME_SPID)
-  if tmPacket.versionNumber != testData.TM_PACKET_02_versionNumber:
-    print "tmPacket versionNumber wrong:", tmPacket.versionNumber, "- should be", testData.TM_PACKET_02_versionNumber
+  # TM_PACKET_03 has a checksum
+  tmPacket = SPACE.IF.s_tmPacketGenerator.getTMpacket(testData.TM_PACKET_03_SPID)
+  print "tmPacket =", tmPacket
+  if tmPacket.versionNumber != testData.TM_PACKET_03_versionNumber:
+    print "tmPacket versionNumber wrong:", tmPacket.versionNumber, "- should be", testData.TM_PACKET_03_versionNumber
     return False
-  if tmPacket.packetType != testData.TM_PACKET_02_packetType:
-    print "tmPacket packetType wrong:", tmPacket.packetType, "- should be", testData.TM_PACKET_02_packetType
+  if tmPacket.packetType != testData.TM_PACKET_03_packetType:
+    print "tmPacket packetType wrong:", tmPacket.packetType, "- should be", testData.TM_PACKET_03_packetType
     return False
-  if tmPacket.dataFieldHeaderFlag != testData.TM_PACKET_02_dataFieldHeaderFlag:
-    print "tmPacket dataFieldHeaderFlag wrong:", tmPacket.dataFieldHeaderFlag, "- should be", testData.TM_PACKET_02_dataFieldHeaderFlag
+  if tmPacket.dataFieldHeaderFlag != testData.TM_PACKET_03_dataFieldHeaderFlag:
+    print "tmPacket dataFieldHeaderFlag wrong:", tmPacket.dataFieldHeaderFlag, "- should be", testData.TM_PACKET_03_dataFieldHeaderFlag
     return False
-  if tmPacket.applicationProcessId != testData.TM_PACKET_02_applicationProcessId:
-    print "tmPacket applicationProcessId wrong:", tmPacket.applicationProcessId, "- should be", testData.TM_PACKET_02_applicationProcessId
+  if tmPacket.applicationProcessId != testData.TM_PACKET_03_applicationProcessId:
+    print "tmPacket applicationProcessId wrong:", tmPacket.applicationProcessId, "- should be", testData.TM_PACKET_03_applicationProcessId
     return False
-  if tmPacket.segmentationFlags != testData.TM_PACKET_02_segmentationFlags:
-    print "tmPacket segmentationFlags wrong:", tmPacket.segmentationFlags, "- should be", testData.TM_PACKET_02_segmentationFlags
+  if tmPacket.segmentationFlags != testData.TM_PACKET_03_segmentationFlags:
+    print "tmPacket segmentationFlags wrong:", tmPacket.segmentationFlags, "- should be", testData.TM_PACKET_03_segmentationFlags
     return False
-  if tmPacket.sequenceControlCount != testData.TM_PACKET_02_sequenceControlCount:
-    print "tmPacket sequenceControlCount wrong:", tmPacket.sequenceControlCount, "- should be", testData.TM_PACKET_02_sequenceControlCount
+  if tmPacket.sequenceControlCount != testData.TM_PACKET_03_sequenceControlCount:
+    print "tmPacket sequenceControlCount wrong:", tmPacket.sequenceControlCount, "- should be", testData.TM_PACKET_03_sequenceControlCount
     return False
-  if tmPacket.packetLength != testData.TM_PACKET_02_packetLength:
-    print "tmPacket packetLength wrong:", tmPacket.packetLength, "- should be", testData.TM_PACKET_02_packetLength
+  if tmPacket.packetLength != testData.TM_PACKET_03_packetLength:
+    print "tmPacket packetLength wrong:", tmPacket.packetLength, "- should be", testData.TM_PACKET_03_packetLength
     return False
-  if tmPacket.pusVersionNumber != testData.TM_PACKET_02_pusVersionNumber:
-    print "tmPacket pusVersionNumber wrong:", tmPacket.pusVersionNumber, "- should be", testData.TM_PACKET_02_pusVersionNumber
+  if tmPacket.pusVersionNumber != testData.TM_PACKET_03_pusVersionNumber:
+    print "tmPacket pusVersionNumber wrong:", tmPacket.pusVersionNumber, "- should be", testData.TM_PACKET_03_pusVersionNumber
     return False
-  if tmPacket.serviceType != testData.TM_PACKET_02_serviceType:
-    print "tmPacket serviceType wrong:", tmPacket.serviceType, "- should be", testData.TM_PACKET_02_serviceType
+  if tmPacket.serviceType != testData.TM_PACKET_03_serviceType:
+    print "tmPacket serviceType wrong:", tmPacket.serviceType, "- should be", testData.TM_PACKET_03_serviceType
     return False
-  if tmPacket.serviceSubType != testData.TM_PACKET_02_serviceSubType:
-    print "tmPacket serviceSubType wrong:", tmPacket.serviceSubType, "- should be", testData.TM_PACKET_02_serviceSubType
+  if tmPacket.serviceSubType != testData.TM_PACKET_03_serviceSubType:
+    print "tmPacket serviceSubType wrong:", tmPacket.serviceSubType, "- should be", testData.TM_PACKET_03_serviceSubType
     return False
   if not tmPacket.checkPacketLength():
     print "tmPacket has inconsistent packetLength"
@@ -87,6 +127,8 @@ def test_TMGENoperations():
 # main #
 ########
 if __name__ == "__main__":
+  # initialise the system configuration
+  UTIL.SYS.s_configuration.setDefaults(SYS_CONFIGURATION)
   print "***** test_DEFoperations() start"
   retVal = test_DEFoperations()
   print "***** test_DEFoperations() done:", retVal
