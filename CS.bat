b5fc5656616a14210d61c4c4986807a914554fb3
Stefan-Korner <korner-hajek@gmx.at>
2018-11-13 01:38:34 +0100

    initial implementation for a Central Checkout and Mission Control system

---
+++ CS.bat
@@ -0,0 +1,17 @@
+::*****************************************************************************
+:: (C) 2018, Stefan Korner, Austria                                           *
+::                                                                            *
+:: The Space Python Library is free software; you can redistribute it and/or  *
+:: modify it under under the terms of the MIT License as published by the     *
+:: Massachusetts Institute of Technology.                                     *
+::                                                                            *
+:: The Space Python Library is distributed in the hope that it will be useful,*
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the MIT License   *
+:: for more details.                                                          *
+::*****************************************************************************
+:: Start scrip for the Control System (CS).                                   *
+::*****************************************************************************
+set TESTENV=C:\Programming\SpacePyLibrary\TESTENV
+set PYTHONPATH=C:\Programming\SpacePyLibrary
+python3 CS.py dummy
---
+++ MCUI/TMgui.py
@@ -0,0 +1,53 @@
+#******************************************************************************
+# (C) 2018, Stefan Korner, Austria                                            *
+#                                                                             *
+# The Space Python Library is free software; you can redistribute it and/or   *
+# modify it under under the terms of the MIT License as published by the      *
+# Massachusetts Institute of Technology.                                      *
+#                                                                             *
+# The Space Python Library is distributed in the hope that it will be useful, *
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the MIT License    *
+# for more details.                                                           *
+#******************************************************************************
+# Monitoring and Control (M&C) - Monitoring (TM) GUI                          *
+#******************************************************************************
+import tkinter
+from UTIL.SYS import Error, LOG, LOG_INFO, LOG_WARNING, LOG_ERROR
+import UI.TKI
+
+###########
+# classes #
+###########
+# =============================================================================
+class GUIview(UI.TKI.GUIwinView):
+  """Implementation of the M&C Monitoring GUI layer"""
+  # ---------------------------------------------------------------------------
+  def __init__(self, master):
+    """Initialise all GUI elements"""
+    UI.TKI.GUIwinView.__init__(self, master, "TM", "M&C Monitoring (TM)")
+    # log messages (default logger)
+    self.messageLogger = UI.TKI.MessageLogger(self, "TM")
+    self.appGrid(self.messageLogger, row=0, columnspan=2)
+    # message line
+    self.messageline = tkinter.Message(self, relief=tkinter.GROOVE)
+    self.appGrid(self.messageline,
+                 row=1,
+                 columnspan=2,
+                 rowweight=0,
+                 columnweight=0,
+                 sticky=tkinter.EW)
+    self.grid(row=0, column=0, sticky=tkinter.EW+tkinter.NS)
+    self.master.rowconfigure(0, weight=1)
+    self.master.columnconfigure(0, weight=1)
+  # ---------------------------------------------------------------------------
+  def fillCommandMenuItems(self):
+    """
+    fill the command menu bar,
+    implementation of UI.TKI.GUIwinView.fillCommandMenuItems
+    """
+    pass
+  # ---------------------------------------------------------------------------
+  def notifyStatus(self, status):
+    """Generic callback when something changes in the model"""
+    LOG_WARNING("TMgui.GUIview.notifyStatus not implemented", "TM")
