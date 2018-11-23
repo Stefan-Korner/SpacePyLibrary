13ce8d1ef3c891d91af2613512729596c599c207
Stefan-Korner <korner-hajek@gmx.at>
2018-11-24 00:41:14 +0100

    support EDEN and CNC protocol in parallel (not finished)

--- CS/EDENclient.py
+++ CS/EDENclient.py
@@ -10,10 +10,8 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the MIT License    *
 # for more details.                                                           *
 #******************************************************************************
-# EGSE client for connection to SCOE                                          *
-# supports one of the following EGSE_PROTOCOLs for SCOE connection:           *
-# - CNC:  implements CAIT-03474-ASTR_issue_3_EGSE_IRD.pdf                     *
-# - EDEN: implements Core_EGSE_AD03_GAL_REQ_ALS_SA_R_0002_EGSE_IRD_issue2.pdf *
+# EDEN client for connection to SCOE                                          *
+# implements Core_EGSE_AD03_GAL_REQ_ALS_SA_R_0002_EGSE_IRD_issue2.pdf         *
 #******************************************************************************
 import sys
 from UTIL.SYS import Error, LOG, LOG_INFO, LOG_WARNING, LOG_ERROR
@@ -24,30 +22,7 @@ import UTIL.SYS
 # classes #
 ###########
 # =============================================================================
-class CNCtcClient(EGSE.CNC.TCclient):
-  """Subclass of EGSE.CNC.TCclient"""
-  # this client sends CnC commands
-  # and receives automatically ACK/NAK CnC responses
-  # ---------------------------------------------------------------------------
-  def __init__(self, hostName, portNr):
-    """Initialise attributes only"""
-    EGSE.CNC.TCclient.__init__(self)
-    self.hostName = hostName
-    self.portNr = portNr
-
-# =============================================================================
-class CNCtmClient(EGSE.CNC.TMclient):
-  """Subclass of EGSE.CNC.TMclient"""
-  # this client only receives CCSDS TM packets
-  # ---------------------------------------------------------------------------
-  def __init__(self, hostName, portNr):
-    """Initialise attributes only"""
-    EGSE.CNC.TMclient.__init__(self)
-    self.hostName = hostName
-    self.portNr = portNr
-
-# =============================================================================
-class EDENclient(EGSE.EDEN.Client):
+class Client(EGSE.EDEN.Client):
   """Subclass of EGSE.EDEN.Client"""
   # this client sends CCSDS TC packets and received CCSDS TM packets
   # ---------------------------------------------------------------------------
@@ -58,7 +33,7 @@ class EDENclient(EGSE.EDEN.Client):
     self.portNr = portNr
 
 # =============================================================================
-class EDENclient(EGSE.EDEN.Client):
+class Client2(EGSE.EDEN.Client):
   """Subclass of EGSE.EDEN.Client"""
   # this client is used for simulating a 2nd client endpoint
   # ---------------------------------------------------------------------------
@@ -71,7 +46,7 @@ class EDENclient(EGSE.EDEN.Client):
 ####################
 # global variables #
 ####################
-# EGSE clients are singletons
+# EDEN clients are singletons
 s_client = None
 s_client2 = None
 
@@ -80,25 +55,16 @@ s_client2 = None
 #############
 # functions to encapsulate access to s_client and s_client2
 # -----------------------------------------------------------------------------
-def createEGSEclients(hostName=None):
+def createClients():
   """create the EGSE clients"""
   global s_client, s_client2
-  egseProtocol = UTIL.SYS.s_configuration.EGSE_PROTOCOL
-  if egseProtocol == "CNC":
-    s_client = CNCtcClient(hostName, portNr=int(UTIL.SYS.s_configuration.SCOE_SERVER_PORT))
-  elif egseProtocol == "EDEN":
-    s_client = EDENclient(hostName, portNr=int(UTIL.SYS.s_configuration.SCOE_SERVER_PORT))
-  else:
-    LOG_ERROR("invalid EGSE_PROTOCOL defined")
-    sys.exit(-1)
-  serverPort2 = int(UTIL.SYS.s_configuration.SCOE_SERVER_PORT2)
-  if serverPort2 > 0:
-    # there is a second server port configured
-    if egseProtocol == "CNC":
-      s_client2 = CNCtmClient(hostName, portNr=serverPort2)
-    elif egseProtocol == "EDEN":
-      s_client2 = EDENclient2(hostName, portNr=serverPort2)
-  else:
-    if egseProtocol == "CNC":
-      LOG_ERROR("CNC protocol requires 2 server ports (TC + TM) configured")
-      sys.exit(-1)
+  edenHost = EGSE.IF.s_edenClientConfiguration.edenHost
+  if edenHost == "":
+    # no EDEN connection configured
+    LOG_INFO
+  edenPort = int(EGSE.IF.s_edenClientConfiguration.edenPort)
+  s_client = Client(edenHost, edenPort)
+  edenPort2 = int(EGSE.IF.s_edenClientConfiguration.edenPort2)
+  if edenPort2 > 0:
+    # there is a second EDEN connection configured
+    s_client2 = Client2(edenHost, edenPort2)
--- CS/EDENgui.py
+++ CS/EDENgui.py
@@ -10,7 +10,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the MIT License    *
 # for more details.                                                           *
 #******************************************************************************
-# EGSE client GUI                                                             *
+# EDEN client GUI                                                             *
 #******************************************************************************
 import tkinter
 from UTIL.SYS import Error, LOG, LOG_INFO, LOG_WARNING, LOG_ERROR
@@ -30,11 +30,11 @@ COLOR_CONNECTED = "#00FF00"
 ###########
 # =============================================================================
 class GUIview(UI.TKI.GUIwinView):
-  """Implementation of the Control System EGSE GUI layer"""
+  """Implementation of the Control System EDEN GUI layer"""
   # ---------------------------------------------------------------------------
   def __init__(self, master):
     """Initialise all GUI elements"""
-    UI.TKI.GUIwinView.__init__(self, master, "EGSE", "EGSE Interface")
+    UI.TKI.GUIwinView.__init__(self, master, "EDEN", "EDEN Interface")
     # menu buttons
     self.menuButtons = UI.TKI.MenuButtons(self,
       [["CONN", self.connectPortCallback, COLOR_BUTTON_FG, COLOR_BUTTON_BG],
@@ -46,33 +46,30 @@ class GUIview(UI.TKI.GUIwinView):
                  columnspan=2,
                  rowweight=0,
                  sticky=tkinter.EW)
-    # EGSE interface status
-    self.egseProtocolField = UI.TKI.ValueField(self, row=1, label="EGSE protocol:")
-    self.egseProtocolField.set(EGSE.IF.s_clientConfiguration.egseProtocol)
-    # SCOE interface host
-    self.scoeHostField = UI.TKI.ValueField(self, row=2, label="SCOE host:")
-    self.scoeHostField.set(EGSE.IF.s_clientConfiguration.scoeHost)
-    # SCOE interface status
-    self.scoeStatusField = UI.TKI.ValueField(self, row=3, label="SCOE interface status:")
-    self.scoeStatusField.set("INIT")
-    self.scoeStatusField.setBackground(COLOR_INITIALISED)
-    # SCOE interface port
-    self.scoePortField = UI.TKI.ValueField(self, row=4, label="SCOE interface port:")
-    self.scoePortField.set(EGSE.IF.s_clientConfiguration.scoePort)
-    # SCOE interface status 2
-    self.scoeStatusField2 = UI.TKI.ValueField(self, row=5, label="SCOE interface status 2:")
-    self.scoeStatusField2.set("INIT")
-    self.scoeStatusField2.setBackground(COLOR_INITIALISED)
-    # S interface port 2
-    self.scoePortField2 = UI.TKI.ValueField(self, row=6, label="SCOE interface port 2:")
-    self.scoePortField2.set(EGSE.IF.s_clientConfiguration.scoePort2)
+    # EDEN interface host
+    self.edenHostField = UI.TKI.ValueField(self, row=1, label="EDEN host:")
+    self.edenHostField.set(EGSE.IF.s_edenClientConfiguration.edenHost)
+    # EDEN interface status
+    self.edenStatusField = UI.TKI.ValueField(self, row=2, label="EDEN interface status:")
+    self.edenStatusField.set("INIT")
+    self.edenStatusField.setBackground(COLOR_INITIALISED)
+    # EDEN interface port
+    self.edenPortField = UI.TKI.ValueField(self, row=3, label="EDEN interface port:")
+    self.edenPortField.set(EGSE.IF.s_edenClientConfiguration.edenPort)
+    # EDEN interface status 2
+    self.edenStatusField2 = UI.TKI.ValueField(self, row=4, label="EDEN interface status 2:")
+    self.edenStatusField2.set("INIT")
+    self.edenStatusField2.setBackground(COLOR_INITIALISED)
+    # EDEN interface port 2
+    self.edenPortField2 = UI.TKI.ValueField(self, row=5, label="EDEN interface port 2:")
+    self.edenPortField2.set(EGSE.IF.s_edenClientConfiguration.edenPort2)
     # log messages
-    self.messageLogger = UI.TKI.MessageLogger(self, "EGSE")
-    self.appGrid(self.messageLogger, row=7, columnspan=2)
+    self.messageLogger = UI.TKI.MessageLogger(self, "EDEN")
+    self.appGrid(self.messageLogger, row=6, columnspan=2)
     # message line
     self.messageline = tkinter.Message(self, relief=tkinter.GROOVE)
     self.appGrid(self.messageline,
-                 row=8,
+                 row=7,
                  columnspan=2,
                  rowweight=0,
                  columnweight=0,
@@ -93,62 +90,62 @@ class GUIview(UI.TKI.GUIwinView):
   # ---------------------------------------------------------------------------
   def connectPortCallback(self):
     """Called when the ConnectPort menu entry is selected"""
-    self.notifyModelTask(["CONNECTPORT"])
+    self.notifyModelTask(["CONNECTEDEN"])
   # ---------------------------------------------------------------------------
   def disconnectPortCallback(self):
     """Called when the DisconnectPort menu entry is selected"""
-    self.notifyModelTask(["DISCONNECTPORT"])
+    self.notifyModelTask(["DISCONNECTEDEN"])
   # ---------------------------------------------------------------------------
   def connectPort2Callback(self):
     """Called when the ConnectPort2 menu entry is selected"""
-    self.notifyModelTask(["CONNECTPORT2"])
+    self.notifyModelTask(["CONNECTEDEN2"])
   # ---------------------------------------------------------------------------
   def disconnectPort2Callback(self):
     """Called when the DisconnectPort2 menu entry is selected"""
-    self.notifyModelTask(["DISCONNECTPORT2"])
+    self.notifyModelTask(["DISCONNECTEDEN2"])
   # ---------------------------------------------------------------------------
   def notifyStatus(self, status):
     """Generic callback when something changes in the model"""
-    if status == "SCOE_CONNECTED":
-      self.scoeConnectedNotify()
-    elif status == "SCOE_DISCONNECTED":
-      self.scoeDisconnectedNotify()
-    elif status == "SCOE2_CONNECTED":
-      self.scoeConnected2Notify()
-    elif status == "SCOE2_DISCONNECTED":
-      self.scoeDisconnected2Notify()
+    if status == "EDEN_CONNECTED":
+      self.edenConnectedNotify()
+    elif status == "EDEN_DISCONNECTED":
+      self.edenDisconnectedNotify()
+    elif status == "EDEN2_CONNECTED":
+      self.edenConnected2Notify()
+    elif status == "EDEN2_DISCONNECTED":
+      self.edenDisconnected2Notify()
   # ---------------------------------------------------------------------------
-  def scoeConnectedNotify(self):
-    """Called when the SCOE port connect function is successfully processed"""
-    self.scoeStatusField.set("CONNECTED")
-    self.scoeStatusField.setBackground(COLOR_CONNECTED)
+  def edenConnectedNotify(self):
+    """Called when the EDEN port connect function is successfully processed"""
+    self.edenStatusField.set("CONNECTED")
+    self.edenStatusField.setBackground(COLOR_CONNECTED)
     self.menuButtons.setState("CONN", tkinter.DISABLED)
     self.menuButtons.setState("DCONN", tkinter.NORMAL)
     self.disableCommandMenuItem("ConnectPort")
     self.enableCommandMenuItem("DisconnectPort")
   # ---------------------------------------------------------------------------
-  def scoeDisconnectedNotify(self):
-    """Called when the SCOE port disconnect function is successfully processed"""
-    self.scoeStatusField.set("DISCONNECTED")
-    self.scoeStatusField.setBackground(COLOR_INITIALISED)
+  def edenDisconnectedNotify(self):
+    """Called when the EDEN port disconnect function is successfully processed"""
+    self.edenStatusField.set("DISCONNECTED")
+    self.edenStatusField.setBackground(COLOR_INITIALISED)
     self.menuButtons.setState("CONN", tkinter.NORMAL)
     self.menuButtons.setState("DCONN", tkinter.DISABLED)
     self.enableCommandMenuItem("ConnectPort")
     self.disableCommandMenuItem("DisconnectPort")
   # ---------------------------------------------------------------------------
-  def scoeConnected2Notify(self):
-    """Called when the SCOE port 2 connect function is successfully processed"""
-    self.scoeStatusField2.set("CONNECTED")
-    self.scoeStatusField2.setBackground(COLOR_CONNECTED)
+  def edenConnected2Notify(self):
+    """Called when the EDEN port 2 connect function is successfully processed"""
+    self.edenStatusField2.set("CONNECTED")
+    self.edenStatusField2.setBackground(COLOR_CONNECTED)
     self.menuButtons.setState("CONN2", tkinter.DISABLED)
     self.menuButtons.setState("DCONN2", tkinter.NORMAL)
     self.disableCommandMenuItem("ConnectPort2")
     self.enableCommandMenuItem("DisconnectPort2")
   # ---------------------------------------------------------------------------
-  def scoeDisconnected2Notify(self):
-    """Called when the SCOE port 2 disconnect function is successfully processed"""
-    self.scoeStatusField2.set("DISCONNECTED")
-    self.scoeStatusField2.setBackground(COLOR_INITIALISED)
+  def edenDisconnected2Notify(self):
+    """Called when the EDEN port 2 disconnect function is successfully processed"""
+    self.edenStatusField2.set("DISCONNECTED")
+    self.edenStatusField2.setBackground(COLOR_INITIALISED)
     self.menuButtons.setState("CONN2", tkinter.NORMAL)
     self.menuButtons.setState("DCONN2", tkinter.DISABLED)
     self.enableCommandMenuItem("ConnectPort2")
