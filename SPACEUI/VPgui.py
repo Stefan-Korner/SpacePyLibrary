6798fa47e0cf35de309711602b92207c83c8e1ac
Stefan-Korner <korner-hajek@gmx.at>
2019-03-16 02:09:25 +0100

    continue implementation of variable packets (backlog #031)

--- SPACEUI/VPgui.py
+++ SPACEUI/VPgui.py
@@ -12,7 +12,6 @@
 #******************************************************************************
 # Variable Packet GUI                                                         *
 #******************************************************************************
-import tkinter
 from tkinter import ttk, simpledialog
 from UTIL.SYS import Error, LOG, LOG_INFO, LOG_WARNING, LOG_ERROR
 import SPACE.IF
